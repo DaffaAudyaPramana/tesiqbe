eeca4c9a3ef8bb213436e1c71b0c8e28cae4804d
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2025-03-09 12:07:33 +0700

    update struct iq question

--- models/iq_question.go
+++ models/iq_question.go
@@ -1,8 +1,14 @@
 package models
 
+import (
+	"time"
+)
+
 type IQQuestion struct {
-	ID        string `json:"id"` // Ubah tipe data menjadi string
-	Question  string `json:"question"`
-	Image     string `json:"image"`
-	AnswerKey string `json:"answer_key"`
+	ID        string    `json:"id"` // Ubah tipe data menjadi string
+	Question  string    `json:"question"`
+	Image     string    `json:"image"`
+	AnswerKey string    `json:"answer_key"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
